b5be17de1142e6790ea15557df79066881f08f15
Francesc Perez <fperez@sklum.com>
2024-01-10 12:52:41 +0100

    fixed

--- src/apps/mooc/backend/routes/courses.route.ts
+++ src/apps/mooc/backend/routes/courses.route.ts
@@ -9,7 +9,7 @@ export const register = (router: Router): void => {
 	const reqSchema = [
 		body('id').isString().withMessage('id must be a string'),
 		body('name').isString().withMessage('name must be a string'),
-		body('duration').isString().withMessage('duration must be a string'),
+		body('duration').isString().withMessage('duration must be a string')
 	];
 
 	const coursePutController = Container.get(CoursePutController);
