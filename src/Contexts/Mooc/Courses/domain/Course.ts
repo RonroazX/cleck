53cf0bdf8a0addada9a698a682fc8323af261f07
Francesc Perez <fperez@sklum.com>
2024-01-16 16:02:17 +0100

    search methood of mongocourserepository finished

--- src/Contexts/Mooc/Courses/domain/Course.ts
+++ src/Contexts/Mooc/Courses/domain/Course.ts
@@ -18,4 +18,24 @@ export class Course {
 		this.name = name;
 		this.duration = duration;
 	}
+
+  toPrimitives(): any {
+    return {
+      id: this.id.value,
+      name: this.name.value,
+      duration: this.duration.value
+    }
+  }
+
+  static fromPrimitives(plainData: {
+    id: string;
+    name: string;
+    duration: string;
+  }): Course {
+    return new Course({
+      id: new CourseId(plainData.id),
+      name: new CourseName(plainData.name),
+      duration: new CourseDuration(plainData.duration),
+    });
+  }
 }
--- src/Contexts/Mooc/Courses/infrastructure/persistance/MongoCourseRepository.ts
+++ src/Contexts/Mooc/Courses/infrastructure/persistance/MongoCourseRepository.ts
@@ -1,8 +1,16 @@
-import { Collection, MongoClient } from 'mongodb';
+import { Collection, MongoClient, ObjectId } from 'mongodb';
 
 import { CourseId } from '../../../Shared/domain/Courses/CourseId';
 import { Course } from '../../domain/Course';
 import { CourseRepository } from '../../domain/CourseRepository';
+import { Nullable } from '../../../../Shared/domain/Nullable';
+
+
+interface CourseDocument {
+  _id: string;
+  name: string;
+  duration: string;
+}
 
 export class MongoCourseRepository implements CourseRepository {
 	private readonly _client: Promise<MongoClient>;
@@ -11,14 +19,25 @@ export class MongoCourseRepository implements CourseRepository {
 	}
 
 	async save(course: Course): Promise<void> {
-		//console.log(this._client);
 		return this.persist(course.id.value, course);
 	}
 
-	public async search(id: CourseId): Promise<void> {}
+	public async search(id: CourseId): Promise<Nullable<Course>> {
+    const collection = await this.collection();
+
+    const document = await collection.findOne<CourseDocument>({_id: new ObjectId(id.value)});
+
+    return document ? Course.fromPrimitives({id: document._id, name: document.name, duration: document.duration}) : null;
+  }
 
 	public async persist(id: string, aggregateRoot: Course): Promise<void> {
-		await this.collection();
+		const collection = await this.collection();
+
+    const objectId = new ObjectId(id);
+
+    const document = {...aggregateRoot.toPrimitives(), _id: objectId, id: undefined};
+
+    await collection.updateOne({_id: objectId}, { $set: document}, {upsert: true})
 	}
 
 	protected collectionName(): string {
